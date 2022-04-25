46ede0a550f4496080acf437b24cdfdf52609dd9
Jagunmolu <kolawolejonathan@gmail.com>
2022-04-25 05:43:51 +0100

    added first solved algorithm

---
+++ 8kyu/multiply.py
@@ -0,0 +1,12 @@
+# This is my first codewars challenge, and it was hard for me then. Right now, I am wondering what the H...!
+# Date: ca October 2021
+# Solution ID: 1
+
+
+"""
+The function below takes in two arguments and is required to return the product of the first and second number.
+"""
+
+
+def multiply(a, b):
+    return a * b
