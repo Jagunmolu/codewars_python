975d86a1b244e9b18f03155c5d589cf7f5d2be9a
Jagunmolu <kolawolejonathan@gmail.com>
2022-04-25 05:49:47 +0100

    added second algorithms

---
+++ 8kyu/convert_a_string_to_a_number.py
@@ -0,0 +1,26 @@
+# This is my second codewars challenge, and it was also hard for me as at when I first attempted it.
+# Right now, I am wondering what the H...!
+# Date: ca January 2022
+# Solution ID: 2
+"""
+The function below takes in an argument: a string which is in number format,
+and it is meant to return an integer.
+
+DESCRIPTION
+We need a function that can transform a string into a number.
+What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string
+is a perfectly valid representation of an integral number.
+"""
+
+
+def string_to_number(s):
+    a = int(s)
+    return a
+
+
+# If I did this right now: it will be as follows:
+
+def string_to_number_updated(s):
+    return int(s)
